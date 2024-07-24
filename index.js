f451f393bef9596ca13181dd7f3547a7c449520a
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-24 10:58:13 -0300

    modulo path con argumentos

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Importacion de Modulos
 const p = require('./module.process.js');
 const { deviceInfo } = require('./module.os.js');
+const { getURLInfo } = require('./module.path.js');
 // Declaraciones
 const {argv} = process;
-
+const [url1, url2, url3] = p.getArgs(argv)
 // Procesos
 p.printArgs(argv);
-console.log(deviceInfo());
+console.log(
+    deviceInfo(),
+    getURLInfo(url1),
+    getURLInfo(url2),
+    getURLInfo(url3)
+);
---
+++ module.path.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+const check = (url) => typeof url === "string"
+
+function getURLInfo(url){
+    return check(url) && path.parse(url)
+    }
+
+function getAbsolutePath(url){
+    return check(url) && path.resolve(url)
+}
+function setAbsolutePath(file){
+    return check(url) && path.join(__dirname, file)
+}
+
+module.exports = {
+    getURLInfo,
+    getAbsolutePath,
+    setAbsolutePath
+}
