d309cacbe4982ee64389427fb53c02c6bad4493c
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-26 12:21:43 -0300

    actualizacion de recursos y peticiones

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const http = require("http");
-const { readFile, read } = require("fs");
+const { readFile } = require("fs");
 const path = require("path");
 
-const fn = (file, res) => readFile(file, (err,data) => res.end(data))
+// Importacion de Elementos
+const Nav = require("./src/nav.js");
+const Header = require("./src/header.js");
+const { Services, Products, Home, ErrorPage } = require("./src/content.js");
 
 const app = http.createServer((req,res) => {
-    const src = "./public/"
-    const {dir, name } = path.parse(req.url)
-    console.log(name);
-    switch(dir.toLowerCase()){
-        case "/":
-        switch (name.toLowerCase()){
-            case "gallery":
-            case "contact":
-                return fn(src+name+".html",res)
-            default: 
-                fn(src+"index.html", res)
-        }   
-        break;
-        case "/styles": 
-            return fn(src+'default.css', res)
-        case "/scripts": 
-            return fn(src+"index.js", res)
-        default: 
-            res.end("Error 404 Pagina No Encontrada")
+    let content = "";
+    let headers = {"Content-Type":"text/html; charset='utf-8'"};
+    const {url, method} = req;
+    res.writeHead(200,headers);
+    res.write(Nav());
+    switch(method){
+    case "GET":
+        if (url.toLowerCase().includes("product")){
+            content = Header("Productos") + Products();
+        }
+        else if(url.toLowerCase().includes("service")){
+            content = Header("Servicios") + Services();
+        }
+        else if(url.toLowerCase().includes("home")){
+            content = Header("Inicio") + Home();
+        }
+        else content = ErrorPage();
+        res.end(content);
+    break;
+    case "POST":
+        if (url.toLowerCase().includes("contact"))
+        readFile("./public/contact.html", (err,data)=> res.end(data));
+    break;
+    case "PUT":
+        readFile("./public/default.css", (err,data) => res.end(data));
+    break;
+    case "DELETE":
+    break;
     }
 })
 
