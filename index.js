60693827aacc22405b617e148590518caca9e6aa
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-29 12:15:20 -0300

    implementar datos simulados y plantillas

--- index.js
+++ index.js
@@ -5,12 +5,54 @@ const app = express(); // Servidor Web
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const msg = `servidor iniciado en ${HOST}:${PORT}`
+// Datos simulados
+const appData = {
+    title: "pescar express", 
+    message: "bienvenido a la pagina principal",
+    links: [ 
+        { text: "inicio", link: "/" },  
+        { text: "perfil", link: "/user/1" },  
+        { text: "publicaciones", link: "/posts" } 
+    ]
+}
+const user = {
+    name: "cristian",
+    email: "cristiandracedo@hotmail.com",
+    address: {
+        street: "calle falsa",
+        number: 123,
+        zipcode: "ABC-321",
+        city: "Springfield"
+    }
+}
+const posts = [
+    {
+        title: "publicacion x", 
+        description:  "nuevo producto",
+        price: "$ 999.9999"
+    },
+    {
+        title: "publicacion y", 
+        description:  "nuevo producto",
+        price: "$ 2999.99"
+    },
+    {
+        title: "publicacion z", 
+        description: "nuevo producto",
+        price: "$ 5999.00"
+    }
+]
+// Config Motor de Plantillas
+app.set("view engine", "pug");
 // Procesamiento de Rutas
-app.get("*", (req, res) => {
-    res.json({
-        title: "pescar express", 
-        message: "bienvenido a la pagina principal",
-    })
+app.get("/", (req, res) => {
+    res.render("index", appData)
+})
+app.get("/user/:id", (req, res) =>{
+    res.render("index", { ...appData, user })
+})
+app.get("/posts", (req, res) => {
+    res.render("index", { ...appData, posts })
 })
 // Inicializacion del Servidor
 app.listen(PORT, HOST, console.log(msg))
