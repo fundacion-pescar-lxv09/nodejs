ef0f5eeba33efd9ba25907ad9d801a4bfab2e551
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-31 10:16:59 -0300

    definir rutas generales y datos simulados

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // Importacion de Modulos
 import express from "express";
+import { appData, user, posts} from "./mockupData.js";
 // Declaraciones
 const app = express(); // Servidor Web
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const msg = `servidor iniciado en ${HOST}:${PORT}`
+// Config Motor de Plantillas
+app.set("view engine", "hbs");
 // Procesamiento de Rutas
-app.get("*", (req, res) => {
-    res.render({
-        title: "pescar express", 
-        message: "bienvenido a la pagina principal",
-    })
+app.get("/", (req, res) => {
+        res.render("index", appData)
 })
+app.get("/user/:id", (req, res) =>{
+    res.render("index", { ...appData, user })
+})
+app.get("/posts", (req, res) => {
+    res.render("index", { ...appData, posts })
+})
+
 // Inicializacion del Servidor
 app.listen(PORT, HOST, console.log(msg))
---
+++ mockupData.js
@@ -0,0 +1,39 @@
+// Datos simulados
+export const appData = {
+    title: "Pescar Express",
+    message: "Bienvenido a la pagina principal",
+    links: [
+        { text: "Inicio", link: "/" },
+        { text: "Perfil", link: "/user/1" },
+        { text: "Publicaciones", link: "/posts" }
+    ],
+    user: false,
+    posts: false
+}
+export const user = {
+    name: "cristian",
+    email: "cristiandracedo@hotmail.com",
+    address: {
+        street: "calle falsa",
+        number: 123,
+        zipcode: "ABC321",
+        city: "Springfield"
+    }
+}
+export const posts = [
+    {
+        title: "publicacion x",
+        description:  "nuevo producto",
+        price: "$ 999.9999"
+    },
+    {
+        title: "publicacion y",
+        description:  "nuevo producto",
+        price: "$ 2999.99"
+    },
+    {
+        title: "publicacion z",
+        description: "nuevo producto",
+        price: "$ 5999.00"
+    }
+]
