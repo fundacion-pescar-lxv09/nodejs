da2b9bbfbf1e443093381f3f666a1224f76a1d79
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-07-31 11:08:10 -0300

    implementar rutas partiales con handlebars

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // Importacion de Modulos
+import path  from "path";
 import express from "express";
+import { engine } from "express-handlebars";
 import { appData, user, posts} from "./mockupData.js";
 // Declaraciones
 const app = express(); // Servidor Web
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const msg = `servidor iniciado en ${HOST}:${PORT}`
+
 // Config Motor de Plantillas
+const config = {
+    partialsDir:  "views/partials",
+    layoutsDir: "views/layouts",
+    defaultLayout: "../index.hbs",
+    extname: "hbs"
+}
+app.engine("hbs", engine(config))
 app.set("view engine", "hbs");
+
 // Procesamiento de Rutas
 app.get("/", (req, res) => {
         res.render("index", appData)
