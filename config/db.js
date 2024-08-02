c78ef29d229a83d9711eaa536d0c4f7f7ac6d8d9
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-02 09:53:20 -0300

    crear conexion con servidor mongodb

---
+++ config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+// Acceso a .env
+dotenv.config()
+const { URI, DB } = process.env
+// Conexion con servidor MongoDB
+const dbConn = () => 
+    mongoose.connect(URI ?? "mongodb://localhost:27017")
+    .then(() => console.log(`conectado con DB ${DB}`))
+    .catch((e) => console.log(`Error: ${e}`))
+// Exportacion de Conexion
+export default dbConn;
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Importacion de Modulos
 import express from "express";
+import morgan from "morgan";
+import dbConn from "./config/db.js";
+
 // Declaraciones
 const app = express(); // Servidor Web
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const msg = `servidor iniciado en ${HOST}:${PORT}`
+// Dependencias de Desarrollo
+app.use(morgan('dev'))
+// Conexion Base de Datos
+dbConn()
 // Procesamiento de Rutas
 app.get("*", (req, res) => {
     res.json({
