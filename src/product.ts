a3f4c630da2e557cec5ac8ccc60ac9885968e25a
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-09 04:58:50 -0300

    definicion de valores predeterminados

--- src/product.ts
+++ src/product.ts
@@ -3,11 +3,11 @@ type cloth = "indumentaria" | "vestimenta" | "ropa" | "moda"
 type category = tech | cloth |"inmuebles"|"automotriz"| "hogar"
 
 class Product {
-    private name:string
-    private description:string
-    private category:category[]
-    private price:number
-    private stock:number
+    private name: string = ''
+    private description: string = ''
+    private category: category[] = [] 
+    private price: number = 0.0000
+    private stock: number = 0
 
     increment(percent:number){
         this.price+= this.price*percent
