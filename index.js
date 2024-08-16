b6d30a0298ffeec88f14645f0677b391e0eae7d0
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-16 10:09:09 -0300

    estructura base proyecto websockets

---
+++ index.js
@@ -0,0 +1,23 @@
+// Dependencias
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+// Variables de Entorno
+const { PORT } = process.env
+// Configuracion Servidor
+const app = express()
+const server = createServer(app);
+const ws = new Server(server);
+// Rutas estaticas
+app.use(express.static('public'));
+// Conexion del Cliente
+ws.on("connection", (socket) => {
+    console.log(`cliente ${socket.id} conectado`);
+    // Eventos Websocket
+
+// Desconexion del Cliente
+    socket.on("disconnect", ()=> {
+        console.log(`cliente ${socket.id} desconectado`);
+    })
+})
+server.listen(PORT ?? 3000, () => console.log(`ejecutando servicio`))
