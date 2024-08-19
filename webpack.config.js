3f112d651aae803bc1566c0efa7415c14fccdb01
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-08-19 11:12:09 -0300

    agregar dependencias adicionales y ajustar configuracion

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,57 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
-
+// Declaraciones
+const dir = process.cwd();
 const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = 'style-loader';
-
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: process.cwd()+'/dist',
-    },
+    // Configuracion del compilador
+    entry: './index.js', // Archivo de Entrada (Principal)
+    output: { path: dir+'/dist' }, // Carpeta de salida
+    // Compatibilidad con Modulos Built-In
+    target: 'node', // Aplicacion NodeJS
+    externals: [nodeExternals()], // Permite importacion al compilar
+    // Servidor para Desarrollo
     devServer: {
-        open: true,
-        host: 'localhost',
+        static: { directory: dir+'/dist' }, // Directorio de Salida
+        open: true, // Apertura automatica
+        host: 'localhost', // Nombre de Dominio
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: dir+'/public/index.html', // Archivo para Vista
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
+        // Reglas de Compilacion
         rules: [
             {
                 test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                loader: 'babel-loader', // Traduccion a VanillaJS
             },
             {
                 test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
+                use: [stylesHandler,'css-loader'], // Interprete CSS
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [stylesHandler, 'css-loader', 'sass-loader'],
+                use: [stylesHandler, 'css-loader', 'sass-loader'], // Compilador SASS
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                type: 'asset', // Identificador de Recursos
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
 };
 
 export default () => {
     if (isProduction) {
-        config.mode = 'production';
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+        config.mode = 'production'; // Entorno de Produccion
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW()); // Genera la configuracion para Aplicacion web Progresiva
     } else {
-        config.mode = 'development';
+        config.mode = 'development'; // Entorno de Desarrollo
     }
     return config;
 };
